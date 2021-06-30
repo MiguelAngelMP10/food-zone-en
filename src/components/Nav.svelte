95aba8f34ae2ae68e7d3f802045220f004250305
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-06-30 12:20:25 -0500

    add component Nav

---
+++ src/components/Nav.svelte
@@ -0,0 +1,63 @@
+<script>
+ 
+</script>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg sticky-top">
+  <div class="container">
+    <img src="./img/Logo foodzone.png" alt="" class="imagen-lo" />
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item">
+          <a href="./" class="nav-link active activo">Home</a>
+        </li>
+        <li class="nav-item" >
+          <a href="colaboradores" class="nav-link active activo"
+            >Colaboradores</a
+          >
+        </li>
+        <li class="nav-item">
+          <a href="nosotros" class="nav-link active activo">Nosotros</a>
+        </li>
+        <li class="nav-item" >
+          <a href="pedir" class="nav-link active activo">Pedido</a>
+        </li>
+        <li class="nav-item" >
+          <a href="noticias" class="nav-link active activo">Noticias</a>
+        </li>
+        <li class="nav-item" >
+          <a href="preguntas-frecuentes" class="nav-link active activo"
+            >Preguntas
+          </a>
+        </li>
+        <li class="nav-item" >
+          <a href="terminos" class="nav-link active activo"
+            >Términos</a
+          >
+        </li>
+        <li class="nav-item">
+          <a href="contacto" class="nav-link active activo">Contacto</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+<style>
+  .imagen-lo{
+    width: 5em;
+    height: auto;
+   
+  }
+ .bg{
+  background-color: #e03c36;
+  
+ }
+ .nav-link{
+  color: white;
+ }
+</style>
